aaa85cda2a355426fd351173ffa498449ce7357d
Ab33l <abell.wandili@strathmore.edu>
2021-04-16 15:15:18 +0300

    Implementation of React, Webpack and Babel

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export class Leads extends Component{
+    render(){
+        return(
+            <div><h1>List Of Lists</h1></div>
+        )
+    }
+}
+
+export default Leads
